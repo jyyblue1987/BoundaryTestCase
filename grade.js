d746e779878cbb35880e8f6bac67e2cbb1f4615d
jyyblue <jyyblue1987@outlook.com>
2017-03-02 16:07:12 +0800

    [add] add exception parameter message.

--- grade.js
+++ grade.js
@@ -1,3 +1,4 @@
+// first digit after the decimal point, i.e., 90.5, or 89.4
 Number.prototype.toFixedDown = function(digits) {
     var re = new RegExp("(\\d+\\.\\d{" + digits + "})(\\d)"),
         m = this.toString().match(re);
@@ -5,8 +6,53 @@ Number.prototype.toFixedDown = function(digits) {
 };
 
 exports.convert = function (score) {
+    if( !score )
+        return 'Undefined Object';
+
+    if( Array.isArray(score) == false )
+        return 'Argument is not array';
+
+    if( score.length < 4 )
+        return 'Argument is smaller than 4';
+
     var grade = 0.1 * score[0] + 0.1 * score[1] + 0.4 * score[2] + 0.4 * score[3];
+
     grade = grade.toFixedDown(1);
 
-    return grade;
+    grade = grade.toFixed(0);
+
+    if( grade >= 93 )
+        return 'A';
+
+    if( grade >= 90 )
+        return 'A-';
+
+    if( grade >= 87 )
+        return 'B+';
+
+    if( grade >= 83 )
+        return 'B';
+
+    if( grade >= 80 )
+        return 'B-';
+
+    if( grade >= 77 )
+        return 'C+';
+
+    if( grade >= 73 )
+        return 'C';
+
+    if( grade >= 70 )
+        return 'C-';
+
+    if( grade >= 67 )
+        return 'D+';
+
+    if( grade >= 63 )
+        return 'D';
+
+    if( grade >= 60 )
+        return 'D-';
+
+    return 'F';
 }
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var grade = require('./grade');
-var score = [1, 2, 3, 4];
+var score = [1, 2, 3];
 var grade = grade.convert(score);
 
 console.log(grade);
